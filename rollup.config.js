b801ce8bfeffd4ec27297865dc43efd5d696fd48
thehaystacker <thehaystacker.in@gmail.com>
2022-08-19 14:07:50 +0530

    Added rollup plugin

---
+++ rollup.config.js
@@ -0,0 +1,4 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
